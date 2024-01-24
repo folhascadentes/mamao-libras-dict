0cfd3a15b2808ce2a6bc3576712f6bc3a29cdcf7
RenanTashiro <renantashiro@hotmail.com>
2024-01-24 14:13:06 -0300

    init: project structure

---
+++ main.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import fitz
+import pytesseract
+
+
+def pdfs_to_imgs(pdf_path, output_image_path):
+    pass
+
+
+def imgs_to_txt(image_path):
+    pass
+
+
+def raw_txt_to_formatted_txt(raw_txt):
+    pass
+
+
+def formatted_txt_to_json():
+    pass
+
+
+def main():
+    pdfs_to_imgs("", "")
+    imgs_to_txt("")
+    raw_txt_to_formatted_txt("")
+    formatted_txt_to_json()
+
+
+if __name__ == "__main__":
+    main()
